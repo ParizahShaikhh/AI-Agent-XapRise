8046b3868f4379de3b787d48b73bae9a1c592031
ParizahShaikhh <137597244+ParizahShaikhh@users.noreply.github.com>
2025-07-09 21:48:44 +0500

    ..

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import json
 
 API_URL = "https://crud-ai-agent.vercel.app/agent/"
 
+
 @cl.on_chat_start
 async def on_chat_start():
     await cl.Message(content="👋 Welcome to the XapRise AI Agent! Ask anything below.").send()
+
 
 @cl.on_message
 async def on_message(message: cl.Message):
